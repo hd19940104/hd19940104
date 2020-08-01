709a1a1915df9710af5085a8f30375c84748f24a
Hd19940104 <houdong@houdongdeMBP.lan>
2020-08-01 17:06:01 +0800

    修改

---
+++ src/main/java/com/zixue/Demo.java
@@ -0,0 +1,20 @@
+package com.zixue;
+
+import java.util.*;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        List  arrayList=new ArrayList();
+        Stack stack = new Stack();
+        Vector vector =new Vector();
+        LinkedList linkedList =new LinkedList();
+
+        HashMap hashMap =new HashMap();
+        HashSet hashSet =new HashSet();
+        Hashtable hashtable =new Hashtable();
+
+
+
+    }
+}
